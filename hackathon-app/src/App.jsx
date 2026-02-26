4e9c0ae8b0d82e3f079c97ef70da69fc8dd34554
Warren Smith <warren.lee.smith@hotmail.co.uk>
2026-02-26 13:18:58 +0000

    INITIAL COMMIT

---
+++ hackathon-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{padding: '2rem', fontFamily: 'sans-serif'}}>
+      <h1>Hackathon App</h1>
+      <p>Anonymous Workplace Appreciation & Wellbeing Tool</p>
+    </div>
+  );
+}
+
+export default App

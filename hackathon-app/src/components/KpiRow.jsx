e74105d49826edde1352be8eb89331c63b38dca2
motogoatUK <software@motogoat.co.uk>
2026-02-28 18:31:42 +0000

    feat: KPI row

--- hackathon-app/src/components/KpiRow.jsx
+++ hackathon-app/src/components/KpiRow.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
-
-export default function KpiRow({ level, quarter, geography }) {
+/*
+ * KpiRow - Key Performance Indicators
+ */
+export default function KpiRow({ level, quarter, geography, row }) {
+	let data_value = [row.wellbeing_index]
 	return (
 		<div className='grid grid-cols-2 md:grid-cols-4 gap-4'>
 			{[
@@ -11,7 +14,7 @@ export default function KpiRow({ level, quarter, geography }) {
 			].map((title) => (
 				<div key={title} className='bg-white/70 rounded-2xl shadow p-4'>
 					<div className='text-xs font-semibold opacity-70'>{title}</div>
-					<div className='text-2xl font-bold mt-2'>—</div>
+					<div className='text-2xl font-bold mt-2'>{data_value}</div>
 					<div className='text-xs mt-2 opacity-60'>
 						{level} • Q{quarter} • {geography || '—'}
 					</div>
